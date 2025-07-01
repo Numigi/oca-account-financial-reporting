e73dc1d5ab4a0eff4dc45a48969e8142722e5285
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2025-07-01 17:49:52 +0100

    TA#68582 [FIX] partner_statement : exclude currency exchange journal's operations

--- partner_statement/report/activity_statement.py
+++ partner_statement/report/activity_statement.py
@@ -115,59 +115,70 @@ class ActivityStatement(models.AbstractModel):
             balance_start[row.pop("partner_id")].append(row)
         return balance_start
 
-    def _display_activity_lines_sql_q1(
-        self, partners, date_start, date_end, account_type
-    ):
+    def _display_activity_lines_sql_q1(self, partners, date_start, date_end,
+                                       account_type):
+        excluded_accounts_ids = tuple(
+            self.env.context.get("excluded_accounts_ids", [])) or (-1,)
+        show_only_overdue = self.env.context.get("show_only_overdue", False)
         payment_ref = _("Payment")
-        return str(
-            self._cr.mogrify(
-                """
-            SELECT m.name AS move_id, l.partner_id, l.date,
-                array_agg(l.id ORDER BY l.id) as ids,
-                CASE WHEN (aj.type IN ('sale', 'purchase'))
-                    THEN l.name
-                    ELSE '/'
-                END as name,
-                CASE
-                    WHEN (aj.type IN ('sale', 'purchase')) AND l.name IS NOT NULL
-                        THEN l.ref
-                    WHEN (aj.type in ('bank', 'cash'))
-                        THEN %(payment_ref)s
-                    ELSE m.ref
-                END as case_ref,
-                l.blocked, l.currency_id, l.company_id,
-                sum(CASE WHEN (l.currency_id is not null AND l.amount_currency > 0.0)
-                    THEN l.amount_currency
-                    ELSE l.debit
-                END) as debit,
-                sum(CASE WHEN (l.currency_id is not null AND l.amount_currency < 0.0)
-                    THEN l.amount_currency * (-1)
-                    ELSE l.credit
-                END) as credit,
-                CASE WHEN l.date_maturity is null
-                    THEN l.date
-                    ELSE l.date_maturity
-                END as date_maturity
-            FROM account_move_line l
-            JOIN account_account aa ON (aa.id = l.account_id)
-            JOIN account_account_type at ON (at.id = aa.user_type_id)
-            JOIN account_move m ON (l.move_id = m.id)
-            JOIN account_journal aj ON (l.journal_id = aj.id)
-            WHERE l.partner_id IN %(partners)s
-                AND at.type = %(account_type)s
-                AND %(date_start)s <= l.date
-                AND l.date <= %(date_end)s
-                AND m.state IN ('posted')
-            GROUP BY l.partner_id, m.name, l.date, l.date_maturity,
-                CASE WHEN (aj.type IN ('sale', 'purchase'))
-                    THEN l.name
-                    ELSE '/'
-                END, case_ref, l.blocked, l.currency_id, l.company_id
-        """,
-                locals(),
-            ),
-            "utf-8",
-        )
+
+        # Exclude currency exchange journal
+        excluded_journal_id = self.env.company.currency_exchange_journal_id.id
+        journal_clause = ""
+        if excluded_journal_id:
+            journal_clause = f"AND l.journal_id != {excluded_journal_id}"
+
+        query = f"""
+               SELECT m.name AS move_id, l.partner_id, l.date,
+                   array_agg(l.id ORDER BY l.id) as ids,
+                   CASE WHEN (aj.type IN ('sale', 'purchase'))
+                       THEN l.name
+                       ELSE '/'
+                   END as name,
+                   CASE
+                       WHEN (aj.type IN ('sale', 'purchase')) AND l.name IS NOT NULL
+                           THEN l.ref
+                       WHEN (aj.type in ('bank', 'cash'))
+                           THEN %(payment_ref)s
+                       ELSE m.ref
+                   END as case_ref,
+                   l.blocked, l.currency_id, l.company_id,
+                   sum(CASE WHEN (l.currency_id is not null AND l.amount_currency > 0.0)
+                       THEN l.amount_currency
+                       ELSE l.debit
+                   END) as debit,
+                   sum(CASE WHEN (l.currency_id is not null AND l.amount_currency < 0.0)
+                       THEN l.amount_currency * (-1)
+                       ELSE l.credit
+                   END) as credit,
+                   CASE WHEN l.date_maturity is null
+                       THEN l.date
+                       ELSE l.date_maturity
+                   END as date_maturity
+               FROM account_move_line l
+               JOIN account_account aa ON (aa.id = l.account_id)
+               JOIN account_account_type at ON (at.id = aa.user_type_id)
+               JOIN account_move m ON (l.move_id = m.id)
+               JOIN account_journal aj ON (l.journal_id = aj.id)
+               WHERE l.partner_id IN %(partners)s
+                   AND at.type = %(account_type)s
+                   AND aa.id not in %(excluded_accounts_ids)s
+                   AND %(date_start)s <= l.date
+                   AND l.date <= %(date_end)s
+                   AND m.state IN ('posted')
+                   {journal_clause} -- <-- CLAUSE AJOUTÉE ICI
+                   AND CASE
+                       WHEN %(show_only_overdue)s
+                       THEN COALESCE(l.date_maturity, l.date) <= %(date_end)s
+                       ELSE TRUE
+                   END
+               GROUP BY l.partner_id, m.name, l.date, l.date_maturity,
+                   CASE WHEN (aj.type IN ('sale', 'purchase'))
+                       THEN l.name
+                       ELSE '/'
+                   END, case_ref, l.blocked, l.currency_id, l.company_id
+           """
+        return str(self._cr.mogrify(query, locals()), "utf-8")
 
     def _display_activity_lines_sql_q2(self, sub, company_id):
         return str(
